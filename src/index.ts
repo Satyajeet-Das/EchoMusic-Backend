b430ccd16f3b0db9ec7a7b236c90490d20be1dca
Satyajeet-Das <dassatyajeet123@gmail.com>
2024-10-07 19:47:37 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.get("/api/test", (req: Request, res: Response) => {
+    res.json({message: "Hello from Express"})
+});
+
+app.listen(7000, () => {
+    console.log("Server Running at localhost:7000")
+});
+
